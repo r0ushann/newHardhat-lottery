1cbdba7cbd984c038d57ea2ab17968046a2c4fe5
r0ushann <roushan007kr@gmail.com>
2022-07-14 11:22:24 +0530

    further

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,15 +1,18 @@
+const {ethers} = require("hardhat")
 const networkConfig = {
     default: {
         name: "hardhat",
         keepersUpdateInterval: "30",
     },
     31337: {
-        name: "localhost",
+        name: "hardhat",
+        enteranceFee:ethers.utils.parseEther("0.01"),
         subscriptionId: "588",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
+        interval:"30"
     },
     4: {
         name: "rinkeby",
@@ -19,6 +22,9 @@ const networkConfig = {
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        interval:"30",
+        enteranceFee:ethers.utils.parseEther("0.01"),
+
     },
     1: {
         name: "mainnet",
--- utils/verify.js
+++ utils/verify.js
@@ -1,21 +1,24 @@
-const {run} = require("hardhat")
+// we can't have these functions in our `helper-hardhat-config`
+// since these use the hardhat library
+// and it would be a circular dependency
+const { run } = require("hardhat")
 
-const verify = async (contractAddress , args) => {
+const verify = async (contractAddress, args) => {
     console.log("Verifying contract...")
-    try{
-        await run("Verify:verify",{
+    try {
+        await run("verify:verify", {
             address: contractAddress,
             constructorArguments: args,
-
         })
-    } catch(e) {
-        if (e.message.toLowerCase().includes("already verified")){
-            console.log("Already Verified!")
-        }
-        else {
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
             console.log(e)
         }
     }
 }
 
-module.exports.tags = {verify};
+module.exports = {
+    verify,
+}
